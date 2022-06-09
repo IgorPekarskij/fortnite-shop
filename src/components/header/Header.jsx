26be51c892b16aee1df3521675256aeaf39c8d84
Igor Pekarsky <igorpekarskij.work@gmail.com>
2022-06-09 17:00:51 +0300

    Fixed public links

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,12 +4,19 @@ export function Header() {
     return (
         <nav>
             <div className="nav-wrapper">
-                <a href="/" className="brand-logo logo-image" target="_blank">
-                    <img src="/logo192.png" alt="Fortnite Shop" width="55px" />
+                <a href="/fortnite-shop/" className="brand-logo logo-image">
+                    <img
+                        src="/fortnite-shop/logo192.png"
+                        alt="Fortnite Shop"
+                        width="55px"
+                    />
                 </a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                     <li>
-                        <a href="https://github.com/IgorPekarskij/fortnite-shop">
+                        <a
+                            href="https://github.com/IgorPekarskij/fortnite-shop"
+                            target="_blank"
+                        >
                             Repo
                         </a>
                     </li>
